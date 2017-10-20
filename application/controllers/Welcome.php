5b6a3e431a48f02609ab0fdec013534a6c6e2332
gracielaclavel <gracy63@gmail.com>
2017-10-19 22:01:28 -0300

    variables
    puntos4y5

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,30 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	function __construct(){
-		parent::__construct();
-		$this->load->helper('form');
-		// llamado a la funcion
-	//	$this->load->model('form_model');  
-		// Un formulario muy complicado
+  function __construct(){
+    parent::__construct();
+    $this->load->helper('form');
+    $this->load->model('form_model');  
+  }
 
-	}
+  public function index()
+  {
+    $this->load->view('formulario');
+  }
+    function validar()
+  { 
+    //echo "Hola<br>";
+    //aqui  estoy creando una funcion  
+    $name = $this->input->post('nombres');
+    $lastname = $this->input->post('apellido1');
+    $Email = $this->input->post('ingresatucorreo');
+    echo "Nombre: ".$name."  Apellido: ".$lastname."correo" .$Email;
+//aqui  estamos  trabajando
+    $data = array('nombre' => $this->input->post('nombres'),'apellido' => $this->input->post('apellido1'), 'Email' => $this->input->post('ingresatucorreo') );
+//aqui  se llama  la  funcion para  guardar   en la  bd
+    $this->form_model->guardar($data);
+  //}
 
-	public function index()
-	{
-		$this->load->view('formulario');
-
-     }
-     function validar()
-{  
-   //vamos a trabajar con formulario eval3
-	$name = $this->input->post('nombre');
-		$lastname = $this->input->post('apellido');
-		$name = $this->input->post('email');
-
-		echo "Nombre: ".$name."  Apellido: ".$lastname .$name. "email";
-		$data = array('nombre'=> $this->input->post('nombre1'),'apellido'=> $this->input->post('apellido1'),'email'=> $this->input->post('email1'));
-$this->form_model->guardar($data);
-	//}
-}
+  }
 }
